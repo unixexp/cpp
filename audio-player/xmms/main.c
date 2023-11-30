dcdd0199c2b7f417816da07070f9d81f4669f473
Yurii Rohoza <unixexp@gmail.com>
2023-11-30 16:16:12 +0200

    Updated makefiles recursion

--- audio-player/xmms/main.c
+++ audio-player/xmms/main.c
@@ -1,7 +1,16 @@
-#include <glib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <getopt.h>
+#include <signal.h>
+#include <ctype.h>
+#include <fcntl.h>
+#ifdef HAVE_SCHED_H
+# include <sched.h>
+#elif defined HAVE_SYS_SCHED_H
+# include <sys/sched.h>
+#endif
+
+#include "xmms.h"
 
 int main() {
 
---
+++ audio-player/xmms/xmms.h
@@ -0,0 +1,24 @@
+#ifndef XMMS_H
+#define XMMS_H
+
+#include <X11/Xlib.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+#include "plugin.h"
+
+#endif
