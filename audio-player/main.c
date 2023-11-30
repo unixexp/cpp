cb2db45d65f4a1040726748ca679c7a8bc7c1499
Yurii Rohoza <unixexp@gmail.com>
2023-11-30 14:55:05 +0200

    updated

--- audio-player/main.c
+++ audio-player/main.c
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +6,6 @@ int main() {
 
     gchar *filename;
     filename = g_strconcat(g_get_home_dir(), "/.xmms", NULL);
-    std::cout << filename << std::endl;
     g_free(filename);
 
 }
