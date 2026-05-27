e3ee46dfd445889469d5cb0262e0de6873f08937
Yurii Rohoza <unixexp@Yuriis-iMac.local>
2026-05-27 15:22:31 +0300

    Prepared basic CoreAudio initialization

---
+++ core-audio/base/build/CMakeFiles/BaseAudioProcessing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BaseAudioProcessing"
+  "BaseAudioProcessing.pdb"
+  "CMakeFiles/BaseAudioProcessing.dir/main.cpp.o"
+  "CMakeFiles/BaseAudioProcessing.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BaseAudioProcessing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ core-audio/base/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <AudioToolbox/AudioToolbox.h>
+
+int main(int argc, const char* argv[]) {
+    // Define audio component
+    AudioComponentDescription desc;
+    desc.componentType = kAudioUnitType_Output;
+    desc.componentSubType = kAudioUnitSubType_HALOutput;
+    desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+    desc.componentFlags = 0;
+    desc.componentFlagsMask = 0;
+
+    AudioComponent comp = AudioComponentFindNext(NULL, &desc);
+    AudioUnit audioUnit;
+    AudioComponentInstanceNew(comp, &audioUnit);
+
+    UInt32 enabledIO = 1;
+    AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Output, 0, &enabledIO, sizeof(enabledIO));
+    AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, 1, &enabledIO, sizeof(enabledIO));
+
+    AudioStreamBasicDescription streamFormat;
+    streamFormat.mSampleRate = 44100.0;
+    streamFormat.mFormatID = kAudioFormatLinearPCM;
+    streamFormat.mFormatFlags = kAudioFormatFlagIsFloat | kAudioFormatFlagIsPacked | kAudioFormatFlagIsNonInterleaved;
+    streamFormat.mBytesPerPacket = 4;
+    streamFormat.mFramesPerPacket = 1;
+    streamFormat.mBytesPerFrame = 4;
+    streamFormat.mChannelsPerFrame = 2; // Stereo
+    streamFormat.mBitsPerChannel = 32;  // float
+
+    AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &streamFormat, sizeof(streamFormat));
+    AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &streamFormat, sizeof(streamFormat));
+
+    AudioUnitInitialize(audioUnit);
+    AudioOutputUnitStart(audioUnit);
+
+    std::cout << "CoreAudio engine started. Press return to exit... " << std::endl;
+    std::cin.get();
+
+    AudioOutputUnitStop(audioUnit);
+    AudioUnitUninitialize(audioUnit);
+    AudioComponentInstanceDispose(audioUnit);
+    //AudioDeviceID defaultDevice = kAudioDeviceUnknown;
+
+    return 0;
+}
+
