83c903d54507dd6613e8d8f84dc8af34ab0c81bf
Yurii Rohoza <unixexp@Yuriis-iMac.local>
2026-05-27 16:26:33 +0300

    No sound. TODO: check and fix

--- core-audio/base/main.cpp
+++ core-audio/base/main.cpp
@@ -16,10 +16,12 @@ int main(int argc, const char* argv[]) {
     AudioUnit audioUnit;
     AudioComponentInstanceNew(comp, &audioUnit);
 
+    // Enable audio input/output
     UInt32 enabledIO = 1;
     AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Output, 0, &enabledIO, sizeof(enabledIO));
     AudioUnitSetProperty(audioUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Input, 1, &enabledIO, sizeof(enabledIO));
 
+    // Define audio format
     AudioStreamBasicDescription streamFormat;
     streamFormat.mSampleRate = 44100.0;
     streamFormat.mFormatID = kAudioFormatLinearPCM;
@@ -29,9 +31,14 @@ int main(int argc, const char* argv[]) {
     streamFormat.mBytesPerFrame = 4;
     streamFormat.mChannelsPerFrame = 2; // Stereo
     streamFormat.mBitsPerChannel = 32;  // float
-
     AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 1, &streamFormat, sizeof(streamFormat));
     AudioUnitSetProperty(audioUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &streamFormat, sizeof(streamFormat));
+
+    // Set render callback to audio data processing
+    AURenderCallbackStruct callbackStruct;
+    callbackStruct.inputProc = myAudioCallback;
+    callbackStruct.inputProcRefCon = NULL;
+    AudioUnitSetProperty(audioUnit, kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Global, 0, &callbackStruct, sizeof(callbackStruct));
 
     AudioUnitInitialize(audioUnit);
     AudioOutputUnitStart(audioUnit);
